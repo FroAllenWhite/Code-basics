7d75bb9c45f1cdeeab8716e5255ec3e1f5e10dec
FroAllenWhite <118559993+FroAllenWhite@users.noreply.github.com>
2023-07-14 21:36:26 +0400

    -

--- code-basics.php
+++ code-basics.php
@@ -1,1 +1,61 @@
 <?php
+
+// 1/75
+echo 'Hello, World!';
+
+// 2/75
+echo 'King in the North!';
+
+// 3/75
+//You know nothing, Jon Snow!
+
+// 4/75
+echo 'Robert';
+echo 'Stannis';
+echo 'Renly';
+
+// 5/75
+echo 9780262531962;
+
+//6 /75
+echo 'What Is Dead May Never Die';
+
+// 7/75
+echo 81/9;
+
+// 8/75
+echo 6 - -81;
+
+// 9/75
+echo 3**5;
+echo -8/-4;
+
+// 10/75
+echo 8 / 2 + 5 - -3 / 2;
+
+// 11/75
+echo 70 * (3 + 4) / (8 + 2);
+
+// 12/75
+echo 5**2 - 3 * 7;
+
+// 13/75
+echo '"Khal Drogo\'s favorite word is "athjahakar""';
+
+// 14/75
+echo "- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".";
+
+// 15/75
+echo "Winter came " . "for the " . "House" . " of Frey.";
+
+// 16/75
+echo chr(126) . chr(94) . chr(37);
+
+// 17/75
+echo -0.304;
+
+// 18/75
+echo '7' - (-8 - -2);
+
+// 19/75
+echo (string)((int)2.9) . " times";
